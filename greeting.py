3965816d9ac7e024a359fd2fe8137b8551ea17a7
Moonika M <moonika.m@tktk.ee>
2026-02-09 00:58:37 +0200

    task completed

--- greeting.py
+++ greeting.py
@@ -3,3 +3,22 @@ Programm tervitab kasutajat vastavalt soole:
 Kui kasutaja valib „m“, väljasta: „Tere, härra [Perekonnanimi]!“
 Kui kasutaja valib „n“, väljasta: „Tere, proua [Perekonnanimi]!“
 Kui kasutaja sisestab midagi muud, väljasta: „Tere tulemast, [Perekonnanimi]! (sugu ei olegi tähtis).“ """
+
+#Alusta programmi.
+#Küsi kasutajalt tema perekonnanime.
+#Küsi kasutajalt tema sugu (m/n).
+#Kui sugu on võrdne "m"-ga, siis väljasta ekraanile "Tere, härra [Perekonnanimi]!".
+#Muidu kui sugu on võrdne "n"-ga, siis väljasta ekraanile "Tere, proua [Perekonnanimi]!".
+#Muidu (kui sisestus ei olnud õige), siis väljasta ekraanile: "Vale väärtus.".
+#Muidu (kui sisestus ei olnud õige), siis väljasta ekraanile: "Tere tulemast, [Perekonnanimi]! (sugu ei olegi tähtis).".
+#Lõpeta programm.
+
+perekonnanimi = input('Sisesta enda perekonnanimi: ')
+sugu = input('Sisesta enda sugu (m/n): ')
+
+if sugu == 'm':
+    print(f'Tere, härra {perekonnanimi}!')
+elif sugu == 'n':
+    print(f'Tere, proua {perekonnanimi}!')
+else:
+    print(f'Tere tulemast, {Perekonnanimi}! (sugu ei olegi tähtis).')
